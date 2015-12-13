527a7fe3f74117a2e436c5ddbcaa8232b92cd9dc
Dreamtaker34 <dreamtaker@gmail.com>
2015-12-12 22:57:42 -0500

    Assignment 44 WIP

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,6 +27,8 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :send_post_created_email
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -43,5 +45,10 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def send_post_created_email
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end
